0b345bb24f14a8ca00d3e38532bb87177b19c46a
Jonathan Carter <joncart@joncart-mac.local>
2015-05-28 22:32:31 -0700

    Refactoring gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var gulp = require("gulp");
-var nodemon = require("gulp-nodemon");
-var sh = require("shelljs");
 var webpack = require("webpack");
 
-// Initialize the client and server build tasks
 ["client", "server"].forEach(function (name) {
     gulp.task("build:" + name, function (done) {
         var config = require("./webpack." + name);
@@ -12,20 +9,24 @@ var webpack = require("webpack");
 });
 
 gulp.task("build", ["build:client", "build:server"]);
+gulp.task("default", ["build"]);
 
-gulp.task("serve:dev:nodemon", ["build"], function () {
-    nodemon({
-        script: "./dist/Server.js",
-        tasks: "build-server",
-        watch: "./src/Server.js"
+if (process.env.NODE_ENV !== "production") {
+    var nodemon = require("gulp-nodemon");
+    var sh = require("shelljs");
+
+    gulp.task("serve:dev:nodemon", ["build"], function () {
+        nodemon({
+            script: "./dist/Server.js",
+            tasks: "build-server",
+            watch: "./src/Server.js"
+        });
     });
-});
 
-gulp.task("serve:dev:webpack", function (done) {
-    // The dev server's Node API doesn't support inline
-    // mode so I'm just using the CLI to keep the config "clean"
-    sh.exec("webpack-dev-server --hot --inline --colors --content-base='./src' --config='webpack.client.js'",
-        function () { done(); });
-});
-
-gulp.task("default", ["serve:dev:nodemon"]);
+    gulp.task("serve:dev:webpack", function (done) {
+        // The dev server's Node API doesn't support inline
+        // mode so I'm just using the CLI to keep the config "clean"
+        sh.exec("webpack-dev-server --hot --inline --colors --content-base='./src' --config='webpack.client.js'",
+            function () { done(); });
+    });
+}
